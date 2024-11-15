792a9978037ab7b0807a5462bdc029563e038c90
Tobias Bergkvist <tbergkvist@ulvetanna.io>
2024-11-15 18:58:53 +0100

    Allow entire card to be clickable instead of just See more...

--- docs/components/Collapsible.tsx
+++ docs/components/Collapsible.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react'
 
 export default function Card({ children }: { children: React.ReactNode }) {
+  const childrenArray = React.Children.toArray(children);
+  const headerContent = childrenArray[0];
+  const bodyContent = childrenArray.slice(1);
+
   const [toggle, setToggle] = React.useState(false);
+
   return (
     <div className="space-y-3 text-md"
       onClick={() => {
         setToggle((toggle) => !toggle);
       }}
     >
-      {toggle ? children : "See more..."}
+      {headerContent}
+      {toggle ? bodyContent : <a>See more...</a>}
     </div>
   );}
