f556d327ef3b53cc5d95caa45c6a02edea5264c5
Matt Heisig <matt@benchmarkapp.io>
2024-12-08 18:25:50 -0800

    Clean up collapsible component in docs

--- docs/components/Collapsible.tsx
+++ docs/components/Collapsible.tsx
@@ -1,19 +1,23 @@
 import * as React from 'react'
+import Chevron from '../public/chevron.svg'
 
 export default function Card({ children }: { children: React.ReactNode }) {
   const childrenArray = React.Children.toArray(children);
   const headerContent = childrenArray[0];
   const bodyContent = childrenArray.slice(1);
 
-  const [toggle, setToggle] = React.useState(false);
+  const [isOpen, setIsOpen] = React.useState(false);
 
   return (
-    <div className="space-y-3 text-md"
-      onClick={() => {
-        setToggle((toggle) => !toggle);
-      }}
-    >
+    <div className="space-y-3 text-md">
       {headerContent}
-      {toggle ? bodyContent : "Click to see more..."}
+      {isOpen && bodyContent}
+
+      <div className="flex justify-center hover:cursor-pointer hover:bg-highlight py-3 transition-colors duration-300"
+        onClick={() => {
+          setIsOpen((isOpen) => !isOpen);
+        }}>
+        <img src={Chevron} className={`transition-transform duration-300 ${isOpen ? '-rotate-90' : 'rotate-90'}`} />
+      </div>
     </div>
   );}
