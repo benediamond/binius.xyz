56b25b3eb804706b39796330e16f8616cd5580e2
Jim Posen <jimpo@ulvetanna.io>
2025-01-09 00:01:51 +0100

    Make OG image URL absolute

--- vocs.config.tsx
+++ vocs.config.tsx
@@ -26,7 +26,7 @@ export default defineConfig({
     light: '/logo-light.svg',
     dark: '/logo-dark.svg',
   },
-  ogImageUrl: '/x-logo.png',
+  ogImageUrl: 'https://www.binius.xyz/x-logo.png',
   markdown: {
     remarkPlugins: [
       remarkMath,
